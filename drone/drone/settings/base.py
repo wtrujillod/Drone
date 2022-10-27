276e3043338de00c4134f5e15d1da32e630cfa35
Wendy Trujillo <wtrujillod@gmail.com>
2022-10-26 21:02:13 -0400

    Otras configuraciones (agregando directorio media", incluyendo "url apps")

--- drone/drone/settings/base.py
+++ drone/drone/settings/base.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -38,11 +38,12 @@ BASE_APP = [
 ]
 
 LOCAL_APP = [
-
+    'apps',
 ]
 
 THIRD_APP = [
     'rest_framework',
+    'simple_history',
 ]
 
 INSTALLED_APPS = BASE_APP + LOCAL_APP + THIRD_APP
@@ -55,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'simple_history.middleware.HistoryRequestMiddleware',
 ]
 
 ROOT_URLCONF = 'drone.urls'
@@ -105,7 +107,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- drone/drone/settings/local.py
+++ drone/drone/settings/local.py
@@ -19,3 +19,7 @@ DATABASES = {
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
